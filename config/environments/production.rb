a9ababbe35a3545dfb1814f37968aa0f5971d619
Leland Reardon <leland6925@gmail.com>
2023-04-04 13:20:55 +0530

    mailer host and port update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['DEVISE_MAILER_USERNAME'],
