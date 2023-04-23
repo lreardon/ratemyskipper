dcd872b1a310acb7f20cdbc582aafe31e74dbd3b
Leland Reardon <leland6925@gmail.com>
2023-04-23 11:04:14 +0545

    Set up Puma, Nginx & Capistrano

--- Gemfile
+++ Gemfile
@@ -77,6 +77,12 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+  gem "capistrano", "~> 3.10", require: false
+  gem "capistrano-rails", "~> 1.6", require: false
+  gem 'capistrano-rvm',     require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano3-puma',   require: false
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,11 +8,12 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+
+  # allow http access to the app without SSL.
+  config.force_ssl = false
+
   # Do not eager load code on boot.
   config.eager_load = false
-
-  # Show full error reports.
-  config.consider_all_requests_local = true
 
   # Enable server timing
   config.server_timing = true
