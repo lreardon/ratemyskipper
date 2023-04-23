3936dc1d110f775ab00466e1ad2ed64d57ad38d5
Leland Reardon <leland6925@gmail.com>
2023-04-23 15:43:14 +0545

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,21 +14,21 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_ed25519.pub) }
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_ed25519.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 
-set :puma_user, fetch(:user)
-set :puma_role, :web
-set :puma_service_unit_env_files, []
-set :puma_service_unit_env_vars, []
+# set :puma_user, fetch(:user)
+# set :puma_role, :web
+# set :puma_service_unit_env_files, []
+# set :puma_service_unit_env_vars, []
 
 ## Defaults:
 # set :scm,           :git
