cf9804aac486d74ff5bf319ba788a7d63ec8e34e
Leland Reardon <leland6925@gmail.com>
2023-04-27 17:26:25 +0545

    signature includes month review was updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
     def show_svg(path)
-        File.open(asset_path(path), "rb") do |file|
-          raw file.read
-        end
+      File.open(asset_path(path), "rb") do |file|
+        raw file.read
       end
+    end
+
+    def readable_datetime_month_year(datetime)
+      datetime.localtime.strftime("%B %Y")
+    end
 end
