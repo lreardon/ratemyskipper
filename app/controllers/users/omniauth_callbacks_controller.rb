f36e7c068e77838764321923d6dbd1e4dba01818
Leland Reardon <leland6925@gmail.com>
2023-04-30 00:38:57 +0545

    omniauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def facebook
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+        # I don't want the flash messages in here, but I don't want to forget about this 'is_navigational_format?' method.
+        # set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+        redirect_to new_user_registration_url
+      end
+    end
+  def failure
+      redirect_to root_path
+    end
+  end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-    def show_svg(path)
-      File.open(asset_path(path), "rb") do |file|
-        raw file.read
-      end
+  def show_svg(path)
+    File.open(asset_path(path), "rb") do |file|
+      raw file.read
     end
+  end
 
-    def readable_datetime_month_year(datetime)
-      datetime.localtime.strftime("%B %Y")
-    end
+  def readable_datetime_month_year(datetime)
+    datetime.localtime.strftime("%B %Y")
+  end
 end
