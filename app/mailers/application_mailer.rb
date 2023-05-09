2d80d84fd28d16c4aa3f8fe982008d710bb51e8e
Leland Reardon <leland6925@gmail.com>
2023-05-09 18:23:32 +0545

    changed typo in email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-	default from: "Leland Reardon <leland@skipperbuoy.com>"
-	layout "mailer"
+	default from: 'Leland Reardon <leland@skipperbuoy.com>'
+	layout 'mailer'
 
 	def invite_email
 		@user = params[:from_user]
-		# @message = params[:message]
-		puts 'ABOUT TO SEND THE EMAIL'
 		mail(to: params[:email], subject: "#{@user.firstname} has invited you to SkipperBuoy!")
-		puts 'sent to'
-		puts params[:email] 
 	end
 
 	def contact_email
