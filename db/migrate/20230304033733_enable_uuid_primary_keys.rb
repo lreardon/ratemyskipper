20c8545e28802233b0e82d10c0aba9f9358e1117
Leland Reardon <leland6925@gmail.com>
2023-05-19 09:04:15 +0545

    reformat migration

--- db/migrate/20230304033733_enable_uuid_primary_keys.rb
+++ db/migrate/20230304033733_enable_uuid_primary_keys.rb
@@ -1,5 +1,4 @@
 class EnableUuidPrimaryKeys < ActiveRecord::Migration[7.0]
-  # This extension was enabled by a system administrator on heliohost, the current production hosting platform
-    enable_extension 'pgcrypto' unless (ENV['RAILS_ENV'] == 'production' or extension_enabled?('pgcrypto'))
-  end
+	# This extension was enabled by a system administrator on heliohost, the current production hosting platform
+	enable_extension 'pgcrypto' unless ENV['RAILS_ENV'] == 'production' || extension_enabled?('pgcrypto')
 end
