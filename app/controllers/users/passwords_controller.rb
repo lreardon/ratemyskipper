cb6927215bbd59740cc4d65ae11c00cd38640efb
Leland Reardon <leland6925@gmail.com>
2025-11-21 12:08:39 -0800

    add return to password reste

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -11,6 +11,7 @@ class Users::PasswordsController < Devise::PasswordsController
 	def create
 		if (user = User.find_by(email: params[:user][:email])) && !user.provider.nil?
 			redirect_back fallback_location: new_user_password_path, alert: "The user associated with this email address logs in with #{helpers.display_omniauth_provider(user.provider)}."
+			return
 		end
 		super
 	end
